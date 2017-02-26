9be81f7b76168077fe69f23ba108dc7ec8cc3f45
Ganeshu <ganesh.shankar@bookmyshow.com>
2017-02-27 00:54:04 +0530

    global string fix

--- src/global.js
+++ src/global.js
@@ -3,4 +3,4 @@ export const GAPI_KEY = 'AIzaSyC_HowlHisUiAyQDJCHZlE57eq6onUOXrg'
 
 export const YOUTUBE_SEARCH_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${GAPI_KEY}&videoCategoryId=10&type=video`
 
-export const YOUTUBE_VIDEO_API = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&key=${GAPI_KEY}'
+export const YOUTUBE_VIDEO_API = `https://www.googleapis.com/youtube/v3/videos?part=snippet&key=${GAPI_KEY}`
