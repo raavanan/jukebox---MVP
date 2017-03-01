da84b6c8b6e7e820549d88e16c432cee58e6a550
Ganeshu <ganesh.shankar@bookmyshow.com>
2017-03-01 12:06:22 +0530

    firebase config removed for git

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,10 +1,5 @@
 import firebase from 'firebase'
-
-const config = {
-  apiKey: "AIzaSyB8N5071FSX3C63QA6seaegn-bRli6ep3g",
-    authDomain: "youtube-jukebox-a1d3f.firebaseapp.com",
-    databaseURL: "https://youtube-jukebox-a1d3f.firebaseio.com"
-};
+import {config} from './config/firebaseConfig'
 
 firebase.initializeApp(config)
 export const Database = firebase.database()
